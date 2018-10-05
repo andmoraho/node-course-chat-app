ce5e5b5fe9cd0d2933ca511dd1a4c0da42ed3189
Andres Morales <andmoraho@gmail.com>
2018-10-05 10:03:01 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'andres',
-        text: 'Hi this is Andres'
-    });
 });
 
 socket.on('disconnect', function() {
